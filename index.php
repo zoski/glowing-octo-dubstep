490a14720bcffc57a0782aa63c102431f07ca7d5
zoski <zoski@free.fr>
2014-05-27 15:35:45 +0200

    css okay

--- index.php
+++ index.php
@@ -9,10 +9,16 @@
     	<?php include 'includes/navbar.php' ?>
     	
     	<h1>Titre trop cool</h1>
+    	
     	<aside>
     		<img src="img/minecraft.png" alt="ma photo d'id ou pas"/>
      	</aside>
-     	<article id="accroche">
-    		You wanna know how I got them? So I had a wife. She was beautiful, like you, who tells me I worry too much, who tells me I ought to smile more, who gambles and gets in deep with the sharks. Hey. One day they carve her face. And we have no money for surgeries. She can't take it. I just wanna see her smile again. I just want her to know that I don't care about the scars. So, I do this to myself. And you know what? She can't stand the sight of me. She leaves. Now I see the funny side. Now I'm always smiling.     	
+     	<article class="accroche">
+    		<p>You wanna know how I got them? So I had a wife. She was beautiful, like you, who tells me I worry too much, who tells me I ought to smile more, who gambles and gets in deep with the sharks.</p>
+    		<p>You wanna know how I got them? So I had a wife. She was beautiful, like you, who tells me I worry too much, who tells me I ought to smile more, who gambles and gets in deep with the sharks.</p>	
     	</article>
+    	
+       <!-- <iframe src="img/cv.pdf" width="100%" height="800px"></iframe>-->
+        
+      
 		<?php include 'includes/footer.php' ?>
